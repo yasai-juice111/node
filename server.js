6a140ee21c57ebbf030117fcdaf08d91007bb7fe
cho_takumi <cho_takumi@grenge.co.jp>
2016-04-12 21:06:52 +0900

    create mysql accesser

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
+// third party
 var express = require('express');
 var path = require('path');
+var http = require('http');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
+// global変数登録
+require('./lib/util/global');
+
 // view engine setup
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -21,6 +24,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+var routes = require('./routes/index');
+var users = require('./routes/users');
 app.use('/', routes);
 app.use('/users', users);
 
@@ -56,7 +61,8 @@ app.use(function(err, req, res, next) {
 });
 
 // listen
-
-app.listen(3000);
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
 
 module.exports = app;
