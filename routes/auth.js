d239833784b39cc78bbbd956c147d47742c85f44
cho_takumi <cho_takumi@grenge.co.jp>
2016-04-24 16:58:13 +0900

    Fixed auth

--- routes/auth.js
+++ routes/auth.js
@@ -10,11 +10,11 @@ var router = express.Router();
  * @param {Function} next ネクスト
  */
 router.get('/', function(req, res, next) {
-    var redirectUrl = '/top';
-    if (mode == "local") {
-        res.redirect(redirectUrl);
-        return;
-    }
+    // var redirectUrl = '/top';
+    // if (mode == "local") {
+    //     res.redirect(redirectUrl);
+    //     return;
+    // }
     redirectUrl = casso.authUrl+
                 "?response_type=code&client_id="+casso.clientId+
                 "&redirect_uri="+casso.callbackUrl+
@@ -25,20 +25,37 @@ router.get('/', function(req, res, next) {
 
 router.get('/callback', function(req, res, next) {
     var code = req.param('code');
+    var error = req.param('error');
+    if (error) {
+        res.redirect('/auth');
+        return;
+    }
 
     http.post(casso.oauthTokenUrl)
-    .set('Content-Type', 'application/x-www-form-urlencoded')
-    .send({"grant_type" : "authorization_code"})
-    .send({"code" : code})
-    .send({"client_id" : casso.clientId})
-    .send({"client_secret" : casso.clientSecret})
-    .send({"grant_type" : "authorization_code"})
-    .send({"redirect_uri" : casso.callbackUrl})
-    .end(function(error, res){
-        console.log('//////////');
-        console.log(error);
-        console.log(res);
-        console.log('//////////');
+    .send("grant_type=authorization_code")
+    .send("code="+code)
+    .send("client_id="+casso.clientId)
+    .send("client_secret="+casso.clientSecret)
+    .send("redirect_uri="+casso.callbackUrl)
+    .end(function(error, responseData) {
+        if (error || responseData.statusCode != 200) {
+            res.redirect('/error');
+            return;
+        }
+        console.log('%%%%%%%%%%%%');
+        console.log(responseData.body);
+        console.log('%%%%%%%%%%%%');
+        if (responseData.body && responseData.body.access_token) {
+            http.get(casso.userInfoUrl)
+            .query("access_token="+responseData.body.access_token)
+            .end(function(error, result) {
+                console.log('//////////');
+                console.log(error);
+                console.log(result);
+                console.log('//////////');
+            });
+        }
+
     });
 });
 
