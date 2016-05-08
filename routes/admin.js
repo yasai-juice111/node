414759ed27bca6f7558754a5830b6f6fa36c8ba1
cho_takumi <cho_takumi@grenge.co.jp>
2016-05-09 04:41:46 +0900

    Fixed admin

--- routes/admin.js
+++ routes/admin.js
@@ -27,4 +27,49 @@ router.get('/', function(req, res, next) {
 	});
 });
 
+/**
+ * 詳細
+ *
+ * @param {Object} req リクエスト
+ * @param {Object} res レスポンス
+ * @param {Function} next ネクスト
+ */
+router.get('/detail', function(req, res, next) {
+
+	var userLunchBoxId = req.param('id');
+
+	adminFacade.detail(req, {
+		"userLunchBoxId": userLunchBoxId
+	},function(error, result) {
+		if (error) {
+		  	res.redirect('/error');
+			return
+		}
+		console.log(result);
+		res.render('admin/detail', result);
+	});
+});
+
+/**
+ * 受取
+ *
+ * @param {Object} req リクエスト
+ * @param {Object} res レスポンス
+ * @param {Function} next ネクスト
+ */
+router.post('/receive', function(req, res, next) {
+
+	var userLunchBoxId = req.param('id');
+
+	adminFacade.receive(req, {
+		"userLunchBoxId": userLunchBoxId
+	},function(error, result) {
+		if (error) {
+		  	res.redirect('/error');
+			return
+		}
+		res.render('admin/receive', result);
+	});
+});
+
 module.exports = router;
