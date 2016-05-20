1efecdc8d4cd489b23c646e87570b4760898d23a
cho_takumi <cho_takumi@grenge.co.jp>
2016-05-20 18:34:02 +0900

    Fixed

--- routes/download.js
+++ routes/download.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('download', { title: 'download' });
+  res.render('download', { layout: 'layout/base_without_footer' });
 });
 
 module.exports = router;
